4e541ebdaeef86b334138f29478aa2f31b5d2641
souranch <93600523+souranch@users.noreply.github.com>
2023-03-26 15:20:55 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,105 @@
+var mysong = document.getElementById("mysong");
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    if(mysong.paused){
+        mysong.play();
+        icon.src = "images/pause.png";
+    }else{
+        mysong.pause();
+        icon.src = "images/play.png";
+    }
+}
+
+var mysong1 = document.getElementById("mysong1");
+var icon1 = document.getElementById("icon1");
+icon1.onclick = function(){
+    if(mysong1.paused){
+        mysong1.play();
+        icon1.src = "images/pause.png";
+    }else{
+        mysong1.pause();
+        icon1.src = "images/play.png";
+    }
+}
+var mysong2 = document.getElementById("mysong2");
+var icon2 = document.getElementById("icon2");
+icon2.onclick = function(){
+    if(mysong2.paused){
+        mysong2.play();
+        icon2.src = "images/pause.png";
+    }else{
+        mysong2.pause();
+        icon2.src = "images/play.png";
+    }
+}
+var mysong3 = document.getElementById("mysong3");
+var icon3 = document.getElementById("icon3");
+icon3.onclick = function(){
+    if(mysong3.paused){
+        mysong3.play();
+        icon3.src = "images/pause.png";
+    }else{
+        mysong3.pause();
+        icon3.src = "images/play.png";
+    }
+}
+
+var mysong4 = document.getElementById("mysong4");
+var icon4 = document.getElementById("icon4");
+icon4.onclick = function(){
+    if(mysong4.paused){
+        mysong4.play();
+        icon4.src = "images/pause.png";
+    }else{
+        mysong4.pause();
+        icon4.src = "images/play.png";
+    }
+}
+
+var mysong5 = document.getElementById("mysong5");
+var icon5 = document.getElementById("icon5");
+icon5.onclick = function(){
+    if(mysong5.paused){
+        mysong5.play();
+        icon5.src = "images/pause.png";
+    }else{
+        mysong5.pause();
+        icon5.src = "images/play.png";
+    }
+}
+
+var mysong6 = document.getElementById("mysong6");
+var icon6 = document.getElementById("icon6");
+icon6.onclick = function(){
+    if(mysong6.paused){
+        mysong6.play();
+        icon6.src = "images/pause.png";
+    }else{
+        mysong6.pause();
+        icon6.src = "images/play.png";
+    }
+}
+
+var mysong7 = document.getElementById("mysong7");
+var icon7 = document.getElementById("icon7");
+icon7.onclick = function(){
+    if(mysong7.paused){
+        mysong7.play();
+        icon7.src = "images/pause.png";
+    }else{
+        mysong7.pause();
+        icon7.src = "images/play.png";
+    }
+}
+
+var mysong8 = document.getElementById("mysong8");
+var icon8 = document.getElementById("icon8");
+icon8.onclick = function(){
+    if(mysong8.paused){
+        mysong8.play();
+        icon8.src = "images/pause.png";
+    }else{
+        mysong8.pause();
+        icon8.src = "images/play.png";
+    }
+}
